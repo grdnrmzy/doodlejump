f8fefdbf3a3344c784476c7a3e9fc118e3b2c859
Yana <dvoryansakaya.yana@gmail.com>
2022-01-02 18:10:24 +0200

    init

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Player : MonoBehaviour {
+
+    public float movementSpeed = 10f;
+
+    private float movement = 0f;
+
+    Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        movement = Input.GetAxis("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 velocity = rb.velocity;
+        velocity.x = movement * movementSpeed;
+        rb.velocity = velocity;
+    }
+}
